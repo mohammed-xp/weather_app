c6aa2ea5a757dd0a2c065eb8fb76b300df117a3d
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-02-02 10:09:45 +0200

    Save my applications as my learn - v2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,27 +1,18 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:weather_app/main.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_cubit.dart';
 import 'package:weather_app/models/weather_model.dart';
 import 'package:weather_app/pages/search_page.dart';
-import 'package:weather_app/providers/weather_provider.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+import '../cubits/weather_cubit/weather_state.dart';
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  void updateUi() {
-    setState(() {});
-  }
-
+class HomePage extends StatelessWidget {
   WeatherModel? weatherData;
+
+  HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    weatherData = Provider.of<WeatherProvider>(context).weatherData;
+    // weatherData = Provider.of<WeatherProvider>(context).weatherData;
 
     return Scaffold(
       appBar: AppBar(
@@ -30,99 +21,137 @@ class _HomePageState extends State<HomePage> {
             onPressed: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) {
                 return SearchPage(
-                  updateUi: updateUi,
-                );
+                    // updateUi: updateUi,
+                    );
               }));
             },
-            icon: Icon(Icons.search),
+            icon: const Icon(Icons.search),
           ),
         ],
-        title: Text('Weather App'),
+        title: const Text('Weather App'),
       ),
-      body: Provider.of<WeatherProvider>(context).weatherData == null
-          ? Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'there is no weather 😔 start',
-                    style: TextStyle(
-                      fontSize: 30,
-                    ),
-                  ),
-                  Text(
-                    'searching now 🔍',
-                    style: TextStyle(
-                      fontSize: 30,
-                    ),
-                  )
-                ],
-              ),
-            )
-          : Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                colors: [
-                  weatherData!.getThemeColor(),
-                  weatherData!.getThemeColor()[300]!,
-                  weatherData!.getThemeColor()[100]!,
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              )),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Spacer(
-                    flex: 3,
-                  ),
-                  Text(
-                    Provider.of<WeatherProvider>(context).cityName!,
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    'updated at : ${weatherData!.date.hour.toString()}:${weatherData!.date.minute.toString()}',
-                    style: TextStyle(
-                      fontSize: 22,
-                    ),
-                  ),
-                  Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Image.asset(weatherData!.getImage()),
-                      Text(
-                        weatherData!.temp.toInt().toString(),
-                        style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          Text('maxTemp :${weatherData!.maxTemp.toInt()}'),
-                          Text('minTemp : ${weatherData!.minTemp.toInt()}'),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Spacer(),
-                  Text(
-                    weatherData!.weatherStateName,
-                    style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Spacer(
-                    flex: 5,
-                  ),
-                ],
-              ),
+      body: BlocBuilder<WeatherCubit, WeatherState>(builder: (context, state) {
+        if (state is WeatherLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is WeatherSuccess) {
+          weatherData = state.weatherModel;
+          return SuccessBody(weatherData: weatherData);
+        } else if (state is WeatherFailure) {
+          return const Center(
+            child: Text('Something went wrong, please try again!!'),
+          );
+        } else {
+          return const DefaultBody();
+        }
+      }),
+    );
+  }
+}
+
+class DefaultBody extends StatelessWidget {
+  const DefaultBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'there is no weather 😔 start',
+            style: TextStyle(
+              fontSize: 30,
             ),
+          ),
+          Text(
+            'searching now 🔍',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SuccessBody extends StatelessWidget {
+  const SuccessBody({
+    super.key,
+    required this.weatherData,
+  });
+
+  final WeatherModel? weatherData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [
+          weatherData!.getThemeColor(),
+          weatherData!.getThemeColor()[300]!,
+          weatherData!.getThemeColor()[100]!,
+        ],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      )),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Spacer(
+            flex: 3,
+          ),
+          Text(
+            BlocProvider.of<WeatherCubit>(context).cityName!,
+            style: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            'updated at : ${weatherData!.date.hour.toString()}:${weatherData!.date.minute.toString()}',
+            style: const TextStyle(
+              fontSize: 22,
+            ),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(weatherData!.getImage()),
+              Text(
+                weatherData!.temp.toInt().toString(),
+                style: const TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Column(
+                children: [
+                  Text('maxTemp :${weatherData!.maxTemp.toInt()}'),
+                  Text('minTemp : ${weatherData!.minTemp.toInt()}'),
+                ],
+              ),
+            ],
+          ),
+          const Spacer(),
+          Text(
+            weatherData!.weatherStateName,
+            style: const TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Spacer(
+            flex: 5,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,60 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:weather_app/models/weather_model.dart';
-import 'package:weather_app/providers/weather_provider.dart';
-import 'package:weather_app/services/weather_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/weather_cubit/weather_cubit.dart';
 
 class SearchPage extends StatelessWidget {
   String? cityName;
-  SearchPage({this.updateUi});
+  SearchPage({super.key, this.updateUi});
   VoidCallback? updateUi;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search a City'),
+        title: const Text('Search a City'),
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: TextField(
-            onChanged: (data)
-            {
-               cityName = data;
+            onChanged: (data) {
+              cityName = data;
             },
             onSubmitted: (data) async {
               cityName = data;
 
-              WeatherService service = WeatherService();
-
-              WeatherModel? weather =
-                  await service.getWeather(cityName: cityName!);
-
-              Provider.of<WeatherProvider>(context,listen: false).weatherData = weather;
-                           Provider.of<WeatherProvider>(context,listen: false).cityName = cityName;
+              BlocProvider.of<WeatherCubit>(context)
+                  .getWeather(cityName: cityName!);
+              BlocProvider.of<WeatherCubit>(context)
+                  .cityName = cityName;
 
               Navigator.pop(context);
             },
             decoration: InputDecoration(
               contentPadding:
-                  EdgeInsets.symmetric(vertical: 32, horizontal: 24),
-              label: Text('search'),
+                  const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+              label: const Text('search'),
               suffixIcon: GestureDetector(
-                
-                onTap : () async 
-                {
-                    WeatherService service = WeatherService();
+                  onTap: () async {
+                    // cityName = data;
 
-              WeatherModel? weather =
-            await service.getWeather(cityName: cityName!);
+                    BlocProvider.of<WeatherCubit>(context)
+                        .getWeather(cityName: cityName!);
+                    BlocProvider.of<WeatherCubit>(context)
+                        .cityName = cityName;
 
-              Provider.of<WeatherProvider>(context,listen: false).weatherData = weather;
-                           Provider.of<WeatherProvider>(context,listen: false).cityName = cityName;
-
-              Navigator.pop(context);
-                },
-                child: Icon(Icons.search)),
-              border: OutlineInputBorder(),
+                    Navigator.pop(context);
+                  },
+                  child: const Icon(Icons.search)),
+              border: const OutlineInputBorder(),
               hintText: 'Enter a city',
             ),
           ),
@@ -63,4 +54,3 @@ class SearchPage extends StatelessWidget {
     );
   }
 }
-
